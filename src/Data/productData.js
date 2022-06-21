82d74a36eaf721210dded470a5ffb9e54dfa1fd1
pritesh511 <94275226+pritesh511@users.noreply.github.com>
2022-06-21 09:26:48 +0530

    add product

--- src/Data/productData.js
+++ src/Data/productData.js
@@ -39,4 +39,64 @@ export const products = [
     qty: 0,
     userEmail: "",
   },
+  {
+    id: 5,
+    name: "Apple Rockit Premium Imported 2 Pc (Approx 120 g - 140 g)",
+    productimgUrl:
+      "https://www.jiomart.com/images/product/150x150/590860404/apple-rockit-premium-imported-2-pc-approx-120-g-140-g-0-20211116.jpg",
+    mrp: 240,
+    price: 199,
+    qty: 0,
+    userEmail: "",
+  },
+  {
+    id: 6,
+    name: "Apple Royal Gala Premium Imported 4 Pc (Approx 750 g - 860 g)",
+    productimgUrl:
+      "https://www.jiomart.com/images/product/150x150/591216980/apple-royal-gala-premium-imported-4-pc-approx-840-g-960-g-0-20220330.jpg",
+    mrp: 240,
+    price: 189,
+    qty: 0,
+    userEmail: "",
+  },
+  {
+    id: 7,
+    name: "Muffets And Tuffets White Bread 400 g",
+    productimgUrl:
+      "https://www.jiomart.com/images/product/150x150/491641380/muffets-and-tuffets-white-bread-400-g-product-images-o491641380-p491641380-0-202203141945.jpg",
+    mrp: 60,
+    price: 30,
+    qty: 0,
+    userEmail: "",
+  },
+  {
+    id: 8,
+    name: "Eveready Red 1015 AA Carbon Zinc Batteries (Pack of 10)",
+    productimgUrl:
+      "https://www.jiomart.com/images/product/150x150/490004996/eveready-red-1015-aa-carbon-zinc-batteries-pack-of-10-product-images-o490004996-p490004996-0-202203170159.jpg",
+    mrp: 170,
+    price: 125,
+    qty: 0,
+    userEmail: "",
+  },
+  {
+    id: 9,
+    name: "Assorted Premium Fruit Pack - Cherry, Blueberry Imported (250 g + 125 g)",
+    productimgUrl:
+      "https://www.jiomart.com/images/product/150x150/591001134/assorted-premium-fruit-pack-cherry-blueberry-250-g-125-g-0-20220512.jpg",
+    mrp: 1000,
+    price: 949,
+    qty: 0,
+    userEmail: "",
+  },
+  {
+    id: 10,
+    name: "Grapes Red Globe Premium Imported Pack 400 g",
+    productimgUrl:
+      "https://www.jiomart.com/images/product/150x150/591043457/grapes-red-globe-premium-imported-pack-400-g-0-20220216.jpg",
+    mrp: 400,
+    price: 329,
+    qty: 0,
+    userEmail: "",
+  },
 ];
